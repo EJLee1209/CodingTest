f5ecfa1c660b1e6620072d8804d0f6eeb86b750f
EJLee1209 <dldmswo1209@gmail.com>
2024-03-09 11:39:28 +0900

    BOJ 11055번 풀이 - DP

--- CodingTest/main.swift
+++ CodingTest/main.swift
@@ -716,4 +716,25 @@ import Foundation
 //Q4963.solution()
 
 //https://www.acmicpc.net/problem/14501
-Q14501.solution()
+//Q14501.solution()
+
+struct Q11055: Template {
+    static func solution() {
+        let n = Int(readLine()!)!
+        let A = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var dp = Array(repeating: 0, count: n)
+        dp[0] = A[0]
+        
+        for i in 1..<n {
+            dp[i] = A[i]
+            
+            for j in 0..<i where A[i] > A[j] {
+                dp[i] = max(dp[i], dp[j] + A[i])
+            }
+        }
+        print(dp.max()!)
+    }
+}
+
+//https://www.acmicpc.net/problem/11055
+Q11055.solution()
